f8a11086b18b7391917c221d5279ac7a816512ef
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-04-11 10:33:50 -0400

    Trained 1000 Epochs of the Model
    Able to balance if it started upright, not if it started lower.

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -15,64 +15,111 @@ class genetic:
         self.numberSteps = numberSteps
 
         self.inputSize = 3
-        self.hiddenSize = 2
+        self.hiddenSize = 6
+        self.numberHiddenLayers = 3
         self.outputSize = 1
         self.population = self.createRandPopulation()
 
+        self.fitnessHistory = []
         
         
 
     
     def createRandWeights(self):
         # Randomly initialize weights between -1 and 1
-        w1 = np.random.uniform(-1, 1, size=(self.inputSize, self.hiddenSize))
-        w2 = np.random.uniform(-1, 1, size=(self.hiddenSize, self.outputSize))
+        weights = []
+        for i in range(self.numberHiddenLayers - 1):
+            if i == 0:
+                weights.append(np.random.uniform(-1, 1, size=(self.inputSize, self.hiddenSize)))
+            elif i == self.numberHiddenLayers - 1:
+                weights.append(np.random.uniform(-1, 1, size=(self.hiddenSize, self.outputSize)))
+            else:
+                weights.append(np.random.uniform(-1, 1, size=(self.hiddenSize, self.hiddenSize)))
 
-        return w1, w2
+        return weights
     
     def createRandPopulation(self):
         created = []
         for i in range(self.populationSize):
-            w1, w2 = self.createRandWeights()
-            created.append(individual(w1, w2, self.mutationRate))
+            weights = self.createRandWeights()
+            created.append(individual(weights, self.mutationRate))
         return created
     
     def crossover(self, bestPerformers): # arithmetic crossover
         newPopulation = []
-        for i in range(self.populationSize):
+        for i in range(self.populationSize - len(bestPerformers)):
             parent1 = bestPerformers[random.randint(0, len(bestPerformers) - 1)]
             parent2 = bestPerformers[random.randint(0, len(bestPerformers) - 1)]
-            w1 = (parent1.w1 + parent2.w1) / 2
-            w2 = (parent1.w2 + parent2.w2) / 2
-            child = individual(w1, w2, self.mutationRate)
+            weights = []
+
+            for i in range(len(parent1.weights)):
+                weights.append((parent1.weights[i] + parent2.weights[i]) / 2)
+            child = individual(weights, self.mutationRate)
             newPopulation.append(child)
         self.population = newPopulation
+    
+    def addBestPerformers(self, bestPerformers):
+        for individual in bestPerformers:
+            self.population.append(individual)
+
 
     def mutatePopulation(self):
         for individual in self.population:
             individual.mutate()
+
+    def rejectOutliers(self, data, m=2):
+        return data[abs(data - np.mean(data)) < m * np.std(data)]
+    
+    def saveModel(self):
+        for i, weight in enumerate(self.population[0].weights):
+            np.save(f'w{i}.npy', self.population[0].weights[0])
+        np.savetxt('data.csv', self.fitnessHistory, delimiter=',')
+        
+        #data = self.rejectOutliers(np.asarray(self.fitnessHistory))
+        self.fitnessHistory.pop(0)
+        plt.plot(self.fitnessHistory)
+        plt.savefig('fitnessHistory.png')
+    
+    def loadModel(self, layers):
+        weights = []
+        for i in range(layers):
+            weights.append(np.load(f'w{i}.npy'))
+            print(f'Loaded {i+1} layers')
+        
+        if len(self.population) == 0:
+            self.population = []
+        for i in range(self.populationSize):
+            self.population.append(individual(weights, self.mutationRate))
     
     def run(self):
-        fitnessHistory = []
+        self.fitnessHistory = []
         for gen in tqdm(range(self.generations)):
             for individual in self.population:
-                initialObs, info = self.env.reset(seed=0) # Seed remains constant so that the same initial observation is used for each individual
-                net = nn.nn(individual.w1, individual.w2)
+                initialObs, info = self.env.reset(seed=100) # Seed remains constant so that the same initial observation is used for each individual
+                net = nn.nn(individual.weights)
                 observation = initialObs
                 for i in (range(self.numberSteps)):
                     nextStep = net(observation)[0]
                     observation, fitness, term, trun, info = self.env.step(nextStep)
                     individual.setFitness(fitness)
-                    # if observation[0][1] > 0:
-                    #     break
-                
-
-            bestPerformers = sorted(self.population, key=lambda x: x.getAverageFitness(), reverse=True)[:int(self.populationSize / 2)]
-            fitnessHistory.append(bestPerformers[0].getAverageFitness())
+                    # if observation[0] < 0:
+                    #     break                
+            bestPerformers = sorted(self.population, key=lambda x: x.getAverageFitness(), reverse=True)[:int(10)]
+            self.fitnessHistory.append(bestPerformers[0].getAverageFitness())
             self.crossover(bestPerformers)
             self.mutatePopulation()
+            #self.addBestPerformers(bestPerformers)
 
-        plt.plot(fitnessHistory)
+    def predict(self, numberSteps, seed):
+        initialObs, info = self.env.reset(seed=seed)
+        net = nn.nn(self.population[0].weights)
+        observation = initialObs
+        for i in (range(numberSteps)):
+            nextStep = net(observation)[0]
+            print(nextStep)
+            observation, fitness, term, trun, info = self.env.step(nextStep)
+            self.env.step(nextStep)
+            self.env.render()
 
                 
 
@@ -83,9 +130,8 @@ class genetic:
 
 class individual:
 
-    def __init__(self, w1, w2, mutationRate):
-        self.w1 = w1
-        self.w2 = w2
+    def __init__(self, weights, mutationRate):
+        self.weights = weights
         self.mutationRate = mutationRate
         self.fitnessHistory = []
 
@@ -97,9 +143,11 @@ class individual:
         return np.average(np.asarray(self.fitnessHistory))
     
     def mutate(self):
-        mutationFactor1 = np.random.uniform(-self.mutationRate, self.mutationRate, size=self.w1.shape)
-        mutationFactor2 = np.random.uniform(-self.mutationRate, self.mutationRate, size=self.w2.shape)
-
-        self.w1 = self.w1 * (1 + mutationFactor1)
-        self.w2 = self.w2 * (1 + mutationFactor2)
+        newWeights = []
+        for weight in self.weights:
+            mutationFactor = np.random.uniform(-self.mutationRate, self.mutationRate, size=weight.shape)
+            newWeight = weight * (1 + mutationFactor)
+            newWeights.append(newWeight)
+        self.weights = newWeights
+                
         
--- nn.py
+++ nn.py
@@ -1,36 +1,41 @@
 import numpy as np
 
 class nn:
-    def __init__(self, w1, w2):
+    def __init__(self, weights):
         # Define input layer size
         self.inputSize = 3
 
         # Define hidden layer size
-        self.hiddenSize = 2
+        self.hiddenSize = 6
+        self.numberHiddenLayers = 3
 
         # Define output layer size
         self.outputSize = 1
-
-        self.w1 = w1
-        self.w2 = w2
+        
+        self.weights = weights
 
 
     def tanh(self, x):
-        return np.tanh(x)
+        return 2 * np.tanh(x)
 
     def __call__(self, input):
         self.input = np.array(input).reshape(1, self.inputSize)
         return self.feedForward()
 
     def feedForward(self):
-        # Hidden layer
-        z1 = self.input.dot(self.w1)
-        a1 = self.tanh(z1)
+        # 1st Hidden layer
+        z = self.input.dot(self.weights[0])
+        a = self.tanh(z)
+
+        # Hidden layers
+        for i in range(1, self.numberHiddenLayers - 1):
+            z = a.dot(self.weights[i])
+            a = self.tanh(z)
 
         # Output layer
-        z2 = a1.dot(self.w2)
-        a2 = self.tanh(z2)
+        z = a.dot(self.weights[-1])
+        y = self.tanh(z)
 
-        return a2
+        return y
 
 
