0a5046a4f7a19a7bc6c97a2a61f110e6aad84963
Sam <58117545+Sammm2607@users.noreply.github.com>
2023-04-11 22:32:37 -0400

    Dimension tweaking

--- nn.py
+++ nn.py
@@ -6,8 +6,8 @@ class nn:
         self.inputSize = 3
 
         # Define hidden layer size
-        self.hiddenSize = 24
-        self.numberHiddenLayers = 3
+        self.hiddenSize = 12
+        self.numberHiddenLayers = 2
 
         # Define output layer size
         self.outputSize = 1
